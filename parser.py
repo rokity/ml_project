530eb998d3f501bd7862543df4a8c4ad89ca84d9
fabsam <samuel.fabrizi97@gmail.com>
2020-01-24 19:14:42 +0100

    add cup parser

--- parser.py
+++ parser.py
@@ -2,16 +2,7 @@ import numpy as np
 from dataset import Dataset
 
 
-class Parser:
-
-    def parse(self, dim_features, dim_out, one_hot=None, perc_val=None):
-        raise NotImplementedError
-
-    def __parse_file(self, path, dim_features, dim_out, one_hot, perc_val):
-        raise NotImplementedError
-
-
-class Monks_parser(Parser):
+class Monks_parser:
     def __init__(self, path_tr, path_ts):
         self.path_tr = path_tr
         self.path_ts = path_ts
@@ -49,9 +40,36 @@ class Monks_parser(Parser):
         if not (perc_val is None):
             n = data.shape[0] - int(data.shape[0] * perc_val)
             tr = Dataset(dim_features, dim_out, data[0:n, :])
-            vl = Dataset(dim_features, dim_out, data[n:data.shape[0], :])
+            vl = Dataset(dim_features, dim_out, data[n:, :])
             return tr, vl
         else:
             return Dataset(dim_features, dim_out, data), None
+
+
+class Cup_parser:
+    def __init__(self, path_tr):
+        self.path_tr = path_tr
+
+    def parse(self, dim_features, dim_out, perc_train=0.5, perc_val=0.25, perc_test=0.25):
+        training_set, validation_set, test_set = self.__parse_file(self.path_tr, dim_features, dim_out, perc_train, perc_val, perc_test)
+        return training_set, validation_set, test_set
+
+    def __parse_file(self, path, dim_features, dim_out, perc_train, perc_val, perc_test):
+        with open(path, 'r') as file:
+            lines = file.readlines()
+            data = np.zeros((len(lines), dim_features + dim_out))
+            i = 0
+            for line in lines[7:]:
+                line = line.strip().split(',')
+                data[i] = line[1:]
+                i += 1
+            file.close()
+        np.random.shuffle(data)
+        n_tr = int(data.shape[0] * perc_train)
+        n_val = n_tr + int(data.shape[0] * perc_val)
+        tr = Dataset(dim_features, dim_out, data[0:n_tr, :])
+        vl = Dataset(dim_features, dim_out, data[n_tr:n_val, :])
+        ts = Dataset(dim_features, dim_out, data[n_val:, :])
+        return tr, vl, ts
 
 
