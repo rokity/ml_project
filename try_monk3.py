aa014976df98a0f83acc94436dbec37d7424b8b7
fabsam <samuel.fabrizi97@gmail.com>
2020-01-28 10:36:17 +0100

    add result monk3 test

--- try_monk3.py
+++ try_monk3.py
@@ -1,6 +1,7 @@
 from functions_factory import FunctionsFactory
 from neural_network import NeuralNetwork
 from parser import *
+import matplotlib.pyplot as plt
 
 # initialize parameters
 path_tr = 'monks/monks-3.train'
@@ -20,12 +21,14 @@ else:
     topology = [one_hot, dim_hid, dim_out]
 
 parser = Monks_parser(path_tr, path_ts)
-tr, vl, ts = parser.parse(dim_in, dim_out, one_hot, 0.3)
+tr, _, ts = parser.parse(dim_in, dim_out, one_hot, None)
 
-nn = NeuralNetwork(topology, f, loss, acc, dim_hid, tr.size, 0.7, 0.3, 0.07)
-err = nn.train(tr, vl, ts, 1e-2, 2000)
+nn = NeuralNetwork(topology, f, loss, acc, dim_hid, tr.size, 0.4, 0.6, 0.01)
+err = nn.train(tr, tr, ts, 1e-2, 2000)
 print("Validation error: {}\n".format(err))
-#nn.save_all_err('./monk_out/3_all_err.png')
-#nn.save_all_acc('./monk_out/3_all_acc.png')
-nn.show_all_err()
-nn.show_all_acc()
+
+#nn.save_trts_err('./monk_out/3_all_err.png')
+#nn.save_trts_acc('./monk_out/3_all_acc.png')
+
+nn.show_trts_err()
+nn.show_trts_acc()
