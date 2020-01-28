7902b55ca5fa0bb1e0c04bb9a4b2cdbc6123ca16
fabsam <samuel.fabrizi97@gmail.com>
2020-01-28 13:58:05 +0100

    fix

--- try_grid_search.py
+++ try_grid_search.py
@@ -1,53 +1,18 @@
-from parser import Parser
 from functions_factory import FunctionsFactory
 from neural_network import NeuralNetwork
+from parser import *
 import numpy as np
 import time
-from threading import Thread
-import os
+import multiprocessing
+import datetime
 
+results=[]
+def run(nn,tr,vl,ts):
+  err = nn.train(tr, vl, ts, 1e-2, 1000)
+  results.append([err,[nn.get_eta(),nn.get_momentum(),nn.get_lambda()]])
 
-class NetworkThread (Thread):
-  
-  def __init__(self,eta,momentum,topology,f_act,loss,dim_hid,tr,ts,vl,lam):
-    Thread.__init__(self)
-    self._eta = eta
-    self._momenutm = momentum
-    self._topology=topology
-    self._f_act=f_act
-    self._loss=loss
-    self._dim_hid=dim_hid
-    self._tr=tr
-    self._ts=ts
-    self._vl=vl
-    self._lambda=lam
-    
-  def run(self):
-    self.nn = NeuralNetwork(self._topology, self._f_act, self._loss, self._dim_hid, self._tr.size, self._eta, self._momenutm,self._lambda)
-    self.err = self.nn.train(self._tr,self._tr, self._ts, 0.02, 2000)    
-    print("fatto")
-
-  def printGraph(self):
-    self.nn.save_trts_err('./out/grid_search_graph/3_trts_err{}.png'.format(self._eta))
-    self.nn.save_trts_acc('./out/grid_search_graph/3_trts_acc{}.png'.format(self._eta))
-  
-  def printError(self):
-      f = open('out/grid_search_error/error.txt', "a")
-      f.write("{}:{} \n".format(self._eta,self.err))     
-      f.close()
-
-def file_error_init():
-    path='out/grid_search_error/error.txt'
-    if (os.path.exists(path)):
-        os.remove(path)
-        with open(path, 'w'): pass
-    else:
-        with open(path, 'w'): pass
-
-
-def main():    
-    n_threads=10
-    file_error_init()
+if __name__ == "__main__":  
+    n_threads=10    
     path_tr = 'monks/monks-3.train'
     path_ts = 'monks/monks-3.test'
     dim_in = 6
@@ -56,27 +21,70 @@ def main():
     dim_out = 1
     f = FunctionsFactory.build('sigmoid')
     loss = FunctionsFactory.build('lms')
+
+    acc = FunctionsFactory.build('accuracy')
     if one_hot is None:
-        topology = [dim_in, dim_hid, dim_out]
+      topology = [dim_in, dim_hid, dim_out]
     else:
-        topology = [one_hot, dim_hid, dim_out]
-    tr, vl, ts = Parser.parse(path_tr, path_ts, dim_in, dim_out, one_hot, None)
-    threads=[]
+      topology = [one_hot, dim_hid, dim_out]
+    parser = Monks_parser(path_tr, path_ts)
+    tr, vl, ts = parser.parse(dim_in, dim_out, one_hot, 0.3)
+    _momentum=float(0.1)
     _eta=float(0.1)
+    _lambda=float(0.01)
+    threads=[]
+    count=0
     for i in range(1,n_threads):
-        threads.append(NetworkThread(_eta,0.5,topology,f,loss,dim_hid,tr,ts,vl,0.01))
-        _eta=round((_eta+0.1),1)  
-    for i in range(1,n_threads):      
-        threads[i-1].start()
-    for i in range(1,n_threads):      
-        threads[i-1].join()
-    # for i in range(1,n_threads):      
-    #     threads[i-1].printGraph()
-    for i in range(1,n_threads):      
-        threads[i-1].printError()    
+        for i in range(1,n_threads):           
+            for i in range(1,n_threads):
+                nn = NeuralNetwork(topology, f, loss, acc, dim_hid, tr.size, _eta, _momentum, _lambda)
+                threads.append(nn)
+                              
+                _lambda=round((_lambda+0.01),2)
+            _momentum=round((_momentum+0.1),1)
+            _lambda=float(0.01)
+        _eta=round((_eta+0.1),1)
+        _momentum=float(0.1)
+        _lambda=float(0.01)
+    process=[]
+    while(count<len(threads)):
+      for i in range(0,10):
+        nn=threads[count]
+        p = multiprocessing.Process(target=run, args=(nn,tr,vl,ts))
+        p.start()
+        print("lancio {}".format(count))
+        process.append(p)
+        count+=1
+      print(datetime.datetime.now())      
+      while(len(process)>0):
+        p=process.pop()
+        p.join()
+        p.close()
+        print("finito {}".format(count))
+      print(datetime.datetime.now())
+
+    
+
+    
+    print("finito")
+    
+   
+    
+    content=""
+    err_min=[1.0,None]
+    for s in results:
+        if(s[0]<err_min[0]):
+          err_min[0]=s[0]
+          err_min[1]=s[1]
+        content+="{} Parametri : Eta -> {} Momentum -> {} Lambda-Regolarizzatore -> {} \n".format(s[0],s[1][0],s[1][1],s[1][2])
+    content+="{} Valore Perfetto : Eta -> {} Momentum -> {} Lambda-Regolarizzatore -> {} \n".format(err_min[0],err_min[1][0],err_min[1][1],err_min[1][2])
+    f = open('out/grid_search_error/error.txt', "a")
+    f.write(content)         
+    f.close()
+
     
 
 
-if __name__ == "__main__":
-    main()
+
+
 
