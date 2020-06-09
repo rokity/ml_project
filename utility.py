e0980f20719e3b32d014fca12ef645bba2ffc2a9
Riccardo Amadio <riccardo.amadio1@gmail.com>
2020-06-09 20:14:23 +0200

    write blidn test

--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib as mpl
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 def write_results(res, best_model, save=None, all=False):
@@ -23,6 +24,23 @@ def write_results(res, best_model, save=None, all=False):
             best_model.save_trts_acc(path_acc)
 
 
-def set_style_plot(style='seaborn', fig_size=(12,10)):
+def set_style_plot(style='seaborn', fig_size=(12, 10)):
     plt.style.use(style)
     mpl.rcParams['figure.figsize'] = fig_size
+
+
+def write_blind_result(results,path):
+    """
+
+    @param results: nparray of 2D dimension to write on csv results
+    @param path: path of csv file
+    @return: boolean success or failure
+    """
+    f = open("out/blind/results.csv", "w")
+    f.write("# Riccardo Aamadio Samuel Fabrizi \n")
+    f.write("# Group Nickname \n")
+    f.write("# ML-CUP19 \n")
+    f.write("# 02/11/2019 \n")
+    for i in range(1,results[0].size+1):
+        f.write("{},{},{}\n".format(i,results[0][i-1],results[1][i-1]))
+    f.close()
