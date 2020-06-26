f2c67e2f7568830581a719550caa9d926f941cc8
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2020-06-26 13:29:40 +0200

    add Adam RMSprop

---
+++ optimizers.py
@@ -0,0 +1,198 @@
+import numpy as np
+
+
+class SGD:
+    def __init__(self, lr=1e-3, mom=0.0, l2=0.0, nesterov=False):
+        self.lr = lr
+        self.momentum = mom
+        self.l2 = l2
+        self.nesterov = nesterov
+        self.is_init = False
+        self.params = dict()
+
+    def initialize(self, layers):
+
+        for layer in layers:
+            name = layer.name
+            self.params["dw_" + name] = np.zeros(layer.w.shape)
+            self.params["db_" + name] = np.zeros(layer.b.shape)
+            if not self.is_init:
+                self.params["vw_" + name] = np.zeros(layer.w.shape)
+                self.params["vb_" + name] = np.zeros(layer.b.shape)
+            if self.nesterov and not self.is_init:
+                self.params["vb_prev_" + name] = np.zeros(layer.b.shape)
+                self.params["vw_prev_" + name] = np.zeros(layer.w.shape)
+        self.is_init = True
+
+    def compute_gradients(self, d, layers):
+        # compute the gradient of the output layer
+        output_l = layers[-1]
+        _, _, y = output_l.cache
+        loc_grad = output_l.loss.compute_der(d, y)
+        for layer in reversed(layers):
+            (x, v, y) = layer.cache
+            partial = loc_grad * layer.f_act.compute_der(v)
+            self.params["dw_" + layer.name] += np.dot(partial, x.T)
+            self.params["db_" + layer.name] += partial
+            loc_grad = np.dot(layer.w.T, partial)
+
+    def get_regualarization_for_loss(self, layers):
+        sum_w = 0
+        for layer in layers:
+            w = layer.w
+            sum_w += np.sum(w*w)
+        return 0.5 * sum_w * self.l2
+
+    def update_parameters(self, layers, batch_size):
+        for layer in layers:
+            dw = self.params["dw_" + layer.name] / batch_size
+            db = self.params["db_" + layer.name] / batch_size
+
+            if self.nesterov:
+                self.params["vw_prev_" + layer.name] = self.params["vw_" + layer.name]
+                self.params["vb_prev_" + layer.name] = self.params["vb_" + layer.name]
+
+            self.params["vw_" + layer.name] = \
+                self.momentum * self.params["vw_" + layer.name] - self.lr * dw
+            self.params["vb_" + layer.name] = \
+                self.momentum * self.params["vb_" + layer.name] - self.lr * db
+
+            if self.nesterov:
+                layer.w += \
+                    self.momentum * self.params["vw_prev_" + layer.name] + \
+                    (1 - self.momentum) * self.params["vw_" + layer.name]
+                layer.b += \
+                    self.momentum * self.params["vb_prev_" + layer.name] + \
+                    (1 - self.momentum) * self.params["vb_" + layer.name]
+            else:
+                layer.w += self.params["vw_" + layer.name]
+                layer.b += self.params["vb_" + layer.name]
+            layer.w -= self.l2 * layer.w
+        self.initialize(layers)
+
+
+class RMSprop:
+    def __init__(self, lr=1e-3, moving_average=0.9, epsilon=1e-7, l2=0.0):
+        self.lr = lr
+        self.moving_avg = moving_average
+        self.epsilon = epsilon
+        self.l2 = l2
+        self.is_init = False
+        self.params = dict()
+
+    def initialize(self, layers):
+
+        for layer in layers:
+            name = layer.name
+            self.params["dw_" + name] = np.zeros(layer.w.shape)
+            self.params["db_" + name] = np.zeros(layer.b.shape)
+            if not self.is_init:
+                self.params["vw_" + name] = np.zeros(layer.w.shape)
+                self.params["vb_" + name] = np.zeros(layer.b.shape)
+        self.is_init = True
+
+    def compute_gradients(self, d, layers):
+        # compute the gradient of the output layer
+        output_l = layers[-1]
+        _, _, y = output_l.cache
+        loc_grad = output_l.loss.compute_der(d, y)
+        for layer in reversed(layers):
+            (x, v, y) = layer.cache
+            partial = loc_grad * layer.f_act.compute_der(v)
+            self.params["dw_" + layer.name] += np.dot(partial, x.T)
+            self.params["db_" + layer.name] += partial
+            loc_grad = np.dot(layer.w.T, partial)
+
+    def get_regualarization_for_loss(self, layers):
+        sum_w = 0
+        for layer in layers:
+            w = layer.w
+            sum_w += np.sum(w*w)
+        return 0.5 * sum_w * self.l2
+
+    def update_parameters(self, layers, batch_size):
+        for layer in layers:
+            dw = self.params["dw_" + layer.name] / batch_size
+            db = self.params["db_" + layer.name] / batch_size
+            self.params["vw_" + layer.name] = \
+                self.moving_avg * self.params["vw_" + layer.name] + (1 - self.moving_avg) * (dw ** 2)
+            self.params["vb_" + layer.name] = \
+                self.moving_avg * self.params["vb_" + layer.name] + (1 - self.moving_avg) * (db ** 2)
+
+            layer.w -= \
+                self.lr * self.params["dw_" + layer.name] / (np.sqrt(self.params["vw_" + layer.name]) + self.epsilon)
+            layer.b -= \
+                self.lr * self.params["db_" + layer.name] / (np.sqrt(self.params["vb_" + layer.name]) + self.epsilon)
+
+            layer.w -= self.l2 * layer.w
+
+        self.initialize(layers)
+
+
+class Adam:
+    def __init__(self, lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-8, l2=0.0):
+        self.lr = lr
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = epsilon
+        self.l2 = l2
+        self.is_init = False
+        self.params = dict()
+
+    def initialize(self, layers):
+        for layer in layers:
+            name = layer.name
+            self.params["dw_" + name] = np.zeros(layer.w.shape)
+            self.params["db_" + name] = np.zeros(layer.b.shape)
+            if not self.is_init:
+                self.params["vw_" + name] = np.zeros(layer.w.shape)
+                self.params["vb_" + name] = np.zeros(layer.b.shape)
+                self.params["mw_" + name] = np.zeros(layer.w.shape)
+                self.params["mb_" + name] = np.zeros(layer.b.shape)
+        self.is_init = True
+
+    def compute_gradients(self, d, layers):
+        # compute the gradient of the output layer
+        output_l = layers[-1]
+        _, _, y = output_l.cache
+        loc_grad = output_l.loss.compute_der(d, y)
+        for layer in reversed(layers):
+            (x, v, y) = layer.cache
+            partial = loc_grad * layer.f_act.compute_der(v)
+            self.params["dw_" + layer.name] += np.dot(partial, x.T)
+            self.params["db_" + layer.name] += partial
+            loc_grad = np.dot(layer.w.T, partial)
+
+    def get_regualarization_for_loss(self, layers):
+        sum_w = 0
+        for layer in layers:
+            w = layer.w
+            sum_w += np.sum(w*w)
+        return 0.5 * sum_w * self.l2
+
+    def update_parameters(self, layers, batch_size):
+        for layer in layers:
+            dw = self.params["dw_" + layer.name] / batch_size
+            db = self.params["db_" + layer.name] / batch_size
+
+            self.params["vw_" + layer.name] = \
+                self.beta_2 * self.params["vw_" + layer.name] + (1 - self.beta_2) * (dw ** 2)
+            self.params["vb_" + layer.name] = \
+                self.beta_2 * self.params["vb_" + layer.name] + (1 - self.beta_2) * (db ** 2)
+
+            self.params["mw_" + layer.name] = \
+                self.beta_1 * self.params["mw_" + layer.name] + (1 - self.beta_1) * dw
+            self.params["mb_" + layer.name] = \
+                self.beta_1 * self.params["mb_" + layer.name] + (1 - self.beta_1) * db
+
+            curr_mw = self.params["mw_" + layer.name] / (1 - self.beta_1)
+            curr_mb = self.params["mb_" + layer.name] / (1 - self.beta_1)
+            curr_vw = self.params["vw_" + layer.name] / (1 - self.beta_2)
+            curr_vb = self.params["vb_" + layer.name] / (1 - self.beta_2)
+
+            layer.w -= \
+                self.lr * ((curr_mw + self.l2 * layer.w) / ((np.sqrt(curr_vw)) + self.epsilon))
+            layer.b -= \
+                self.lr * curr_mb / ((np.sqrt(curr_vb)) + self.epsilon)
+
+        self.initialize(layers)
