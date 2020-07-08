b016237ecacf6061f1c1670cbc082d84816df099
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2020-07-08 21:19:33 +0200

    update Monks results

--- single_monk_test2.py
+++ single_monk_test2.py
@@ -3,19 +3,20 @@ from parser import Monks_parser
 from utility import set_style_plot
 from utility import *
 from kernel_initialization import *
+from optimizers import *
 
 set_style_plot()
 
-DIR_DATA = "/home/fabsam/Documenti/masterDegree/ML/ml_project/data/monks/"
+DIR_DATA = "./data/monks/"
 TR_FILE = 'monks-2.train'
 TS_FILE = 'monks-2.test'
 
 
 path_tr = DIR_DATA + TR_FILE
 path_ts = DIR_DATA + TS_FILE
-path_loss = 'out/monks/monk2/err_monk2.png'
-path_acc = 'out/monks/monk2/acc_monk2.png'
-path_result_model = 'out/monks/monk2/results.csv'
+path_loss = './out/monks/monk2/err_monk2.png'
+path_acc = './out/monks/monk2/acc_monk2.png'
+path_result_model = './out/monks/monk2/results.csv'
 
 dim_in = 6
 one_hot = 17
@@ -30,7 +31,7 @@ Y_test = change_output_value(Y_test, 0, -1)
 
 
 dim_in = one_hot
-dim_hid = 3
+dim_hid = 2
 n_attemps = 5
 
 res_losses = np.zeros(n_attemps)
@@ -42,16 +43,16 @@ res_metrics_ts = np.zeros(n_attemps)
 for i in range(n_attemps):
     model = NeuralNetwork('mse', 'accuracy1-1')
 
-    model.add_layer(dim_hid, input_dim=dim_in, activation='sigmoid', kernel_initialization=RandomUniformInitialization())
-    model.add_output_layer(dim_out, activation='tanh', kernel_initialization=RandomUniformInitialization())
+    model.add_layer(dim_hid, input_dim=dim_in, activation='sigmoid',
+                    kernel_initialization=RandomUniformInitialization())
+    model.add_layer(dim_out, activation='tanh', kernel_initialization=RandomUniformInitialization(-1, 1))
 
-    model.compile(0.6, 0.8)
-    history = model.fit(
-        X_train, Y_train, 500, X_train.shape[0], ts=(X_test, Y_test),
-        verbose=True
-    )
-    #if i == 0:
-    #    write_results(res=None, model=model, save_plot_loss=path_loss, save_plot_metric=path_acc, test=True)
+    model.compile(optimizer=SGD(lr=0.5, mom=0.8))
+    history = model.fit(X_train, Y_train, 130, X_train.shape[0], ts=(X_test, Y_test),
+                        verbose=True
+                        )
+    if i == 0:
+        write_results(res=None, model=model, save_plot_loss=path_loss, save_plot_metric=path_acc, test=True)
     res_losses[i] = history['mse'][-1]
     res_losses_ts[i] = history['test_mse'][-1]
     res_metrics[i] = history['accuracy'][-1]*100
@@ -68,4 +69,4 @@ res = {
 }
 
 print(res)
-#write_results(res, model=None, save_result=path_result_model, test=True)
+write_results(res, model=None, save_result=path_result_model, test=True)
--- single_monk_test3nr.py
+++ single_monk_test3nr.py
@@ -3,19 +3,21 @@ from parser import Monks_parser
 from utility import set_style_plot
 from utility import *
 from kernel_initialization import *
+from optimizers import *
 
 set_style_plot()
 
-DIR_DATA = "/home/fabsam/Documenti/masterDegree/ML/ml_project/data/monks/"
+DIR_DATA = "./data/monks/"
 TR_FILE = 'monks-3.train'
 TS_FILE = 'monks-3.test'
 
 
 path_tr = DIR_DATA + TR_FILE
 path_ts = DIR_DATA + TS_FILE
-path_loss = 'out/monks/monk3/err_monk3nr.png'
-path_acc = 'out/monks/monk3/acc_monk3nr.png'
-path_result_model = 'out/monks/monk3/results_nr.csv'
+path_loss = './out/monks/monk3/err_monk3nr.png'
+path_acc = './out/monks/monk3/acc_monk3nr.png'
+path_result_model = './out/monks/monk3/results_nr.csv'
+
 
 dim_in = 6
 one_hot = 17
@@ -25,10 +27,9 @@ parser = Monks_parser(path_tr, path_ts)
 
 X_train, Y_train, X_test, Y_test = parser.parse(dim_in, dim_out, one_hot)
 
-'''
+
 Y_train = change_output_value(Y_train, 0, -1)
 Y_test = change_output_value(Y_test, 0, -1)
-'''
 
 dim_in = one_hot
 dim_hid = 3
@@ -41,16 +42,17 @@ res_losses_ts = np.zeros(n_attemps)
 res_metrics_ts = np.zeros(n_attemps)
 
 for i in range(n_attemps):
-    model = NeuralNetwork('mse', 'accuracy')
 
-    model.add_layer(dim_hid, input_dim=dim_in, activation='relu', kernel_initialization=RandomUniformInitialization())
-    model.add_output_layer(dim_out, activation='sigmoid', kernel_initialization=RandomUniformInitialization())
+    model = NeuralNetwork('mse', 'accuracy1-1')
 
-    model.compile(0.4, 0.6, 0.0)
-    history = model.fit(
-        X_train, Y_train, 500, X_train.shape[0], ts=(X_test, Y_test),
-        verbose=True
-    )
+    model.add_layer(dim_hid, input_dim=dim_in, activation='sigmoid', kernel_initialization=RandomNormalInitialization())
+    model.add_layer(dim_out, activation='tanh', kernel_initialization=RandomNormalInitialization())
+
+    model.compile(optimizer=SGD(lr=0.5, mom=0.8, l2=0.0))
+
+    history = model.fit(X_train, Y_train, 200, X_train.shape[0], ts=(X_test, Y_test),
+                        verbose=True
+                        )
     if i == 0:
         write_results(res=None, model=model, save_plot_loss=path_loss, save_plot_metric=path_acc, test=True)
     res_losses[i] = history['mse'][-1]
