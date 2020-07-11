f9d13a353574d35daa7464243f06150c23ed562c
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2020-07-11 11:11:03 +0200

    results Cup

--- model_selection_cup.py
+++ model_selection_cup.py
@@ -8,7 +8,7 @@ from model_selection import *
 DIR_CUP = './data/cup/'
 PATH_TR = 'ML-CUP19-TR.csv'
 PATH_TS = 'ML-CUP19-TS.csv'
-path_result_randomsearch='./out/cup/search.csv'
+path_result_randomsearch='./out/cup/search4.csv'
 '''
 path_result_model = './out/cup/grid_search_model.csv'
 path_plot_loss = os.getcwd()+'/out/cup/grid_search_plot_loss.png'
@@ -24,22 +24,22 @@ K = 4
 
 
 PARAM_SEARCH = {
-    'eta': list([0.001, 0.0001, 0.01]),
+    'eta': list([0.001, 0.0001, 0.005]),
     'momentum': list([0.7, 0.8, 0.9]),
-    'lr_linear_decay': list([True, False]),
+    'lr_linear_decay': list([False]),
     'epoch_tau': list([200, 300, 350]),
     'epsilon_tau_perc': list([0.01, 0.02, 0.05]),
-    'lambda': list([0.0002, 0.0004, 0.0005, 0.0009]),
+    'lambda': list([0.0002, 0.0004, 0.0005, 0.0009, 0.001]),
     'add_layer': list([True, False]),
     'hidden_nodes': list([20, 30, 40]),
     'hidden_nodes2': list([20, 30, 40]),
-    'optimizer': list(['SGD']),
+    'optimizer': list(['RMSprop', 'Adam', 'SGD']),
     'activation_function1': list(['tanh', 'sigmoid']),
     'activation_function2': list(['tanh', 'sigmoid']),
-    'init_weights': list(['XavierNormalInitialization','HeInitialization',  'RandomNormalInitialization']),
+    'init_weights': list(['XavierNormalInitialization', 'HeInitialization', 'RandomNormalInitialization']),
     'batch_size': list([8, 16, 32, 64]),
-    #'beta_1': list([0.9, 0.99, 0.999]),
-    #'beta_2': list([0.9, 0.99, 0.999]),
+    'beta_1': list([0.9, 0.99, 0.999]),
+    'beta_2': list([0.9, 0.99, 0.999]),
 }
 
 
@@ -49,8 +49,8 @@ def create_model(hyperparams):
     lr = hyperparams['eta']
     mom = hyperparams['momentum']
     l2 = hyperparams['lambda']
-    #beta_1 = hyperparams['beta_1']
-    #beta_2 = hyperparams['beta_2']
+    beta_1 = hyperparams['beta_1']
+    beta_2 = hyperparams['beta_2']
     act_fun_1 = hyperparams['activation_function1']
     hidden_nodes1 = hyperparams['hidden_nodes']
     kernel_init = getattr(_module_kernelinit, hyperparams['init_weights'])
@@ -63,6 +63,7 @@ def create_model(hyperparams):
         hidden_nodes2 = hyperparams['hidden_nodes2']
         model.add_layer(hidden_nodes2, activation=act_fun_2, kernel_initialization=kernel_init())
     model.add_layer(OUTPUT_DIM, activation='linear', kernel_initialization=kernel_init())
+
     if optimizer_str == "SGD":
         if hyperparams['lr_linear_decay']:
             decay_lr_par = {
@@ -99,9 +100,3 @@ model = random_search(
 )
 
 print(time.time() - start_time, "seconds")
-
-'''
-write_results(res, model=None, save_result=path_result_model, test=False, validation=True)
-model.plot_loss(val=True, test=True, show=False, path=path_plot_loss)
-model.plot_metric(val=True, test=True, show=False, path=path_plot_metric)
-'''
--- single_cup_test.py
+++ single_cup_test.py
@@ -8,28 +8,31 @@ from optimizers import *
 DIR_CUP = './data/cup/'
 PATH_TR = 'ML-CUP19-TR.csv'
 PATH_TS = 'ML-CUP19-TS.csv'
+PATH_MSE = './out/cup/model1_mse.png'
+PATH_MEE = './out/cup/model1_mee.png'
 INPUT_DIM = 20
 OUTPUT_DIM = 2
 
-PERC_VL = 0.25
 PERC_TS = 0.25
+PERC_VL = 0.25
 
 set_style_plot()
 
 parser = Cup_parser(DIR_CUP + PATH_TR)
 data, targets = parser.parse(INPUT_DIM, OUTPUT_DIM)
-X_train, Y_train, X_test, Y_test = train_test_split(data, targets, test_size=PERC_TS, shuffle=True)
+X_train, Y_train, X_val, Y_val, X_test, Y_test = train_val_test_split(data, targets, val_size=PERC_VL, test_size=PERC_TS, shuffle=True)
 
 model = NeuralNetwork('mse', 'mee')
 
-model.add_layer(30, input_dim=X_train.shape[1], activation='tanh', kernel_initialization=XavierNormalInitialization())
-model.add_layer(30, activation='tanh', kernel_initialization=XavierNormalInitialization())
-model.add_layer(Y_train.shape[1], activation='linear', kernel_initialization=XavierNormalInitialization())
+model.add_layer(40, input_dim=X_train.shape[1], activation='tanh', kernel_initialization=RandomNormalInitialization())
+model.add_layer(30, activation='sigmoid', kernel_initialization=RandomNormalInitialization())
+model.add_layer(Y_train.shape[1], activation='linear', kernel_initialization=RandomNormalInitialization())
 
-model.compile(optimizer=RMSprop(lr=0.0001, moving_average=0.9, l2=0.0005))
+model.compile(optimizer=RMSprop(lr=0.0001, moving_average=0.9, l2=0.0004))
 
 model.fit(
-    X_train, Y_train, 500, batch_size=64, ts=(X_test, Y_test),
+    X_train, Y_train, 500, batch_size=64, vl=(X_val, Y_val), ts=(X_test, Y_test),
     shuffle=True, tol=1e-2, verbose=True)
-model.plot_loss(val=False, test=True)
-model.plot_metric(val=False, test=True)
+
+model.plot_loss(val=True, test=True, path=PATH_MSE)
+model.plot_metric(val=True, test=True, path=PATH_MEE)
